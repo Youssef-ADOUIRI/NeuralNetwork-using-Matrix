58a37867ec8a6cb081a6a19330eabd8a8ac89972
Youssef Adouiri Alaoui <86375309+Youssef-ADOUIRI@users.noreply.github.com>
2021-08-28 19:15:22 +0100

    Delete src directory

--- src/Nn.cpp
+++
@@ -1,7 +0,0 @@
-#include "Nn.h"
-
-NeuralNetwork::NeuralNetwork(uint input_nodes_num , uint hidden_nodes_num , uint output_nodes_num) : m_input_nodes_num(input_nodes_num) , m_hidden_nodes_num(hidden_nodes_num) ,  m_output_nodes_num(output_nodes_num)
-{
-
-}
-
